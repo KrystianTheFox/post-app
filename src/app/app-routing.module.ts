9a8ec8612ef4dfd491759c6befddd8444ed94966
Krystian <fox.games@abc.pl>
2021-06-14 17:10:37 +0200

    update 0.0.2 adding stuf and fixing some stuf too

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './component/home/home.component';
 import {AboutComponent} from './component/about/about.component';
+import {PageNotFoundComponent} from './component/page-not-found/page-not-found.component';
+import {PostsComponent} from './component/posts/posts.component';
+import {PhotosComponent} from './component/photos/photos.component';
+import {PostItemComponent} from './component/post-item/post-item.component';
+
 const routes: Routes = [
   {
     path: '',
@@ -15,8 +20,25 @@ const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: 'posts/:id',
+    component: PostItemComponent
+  },
+  {
+    path: 'photos',
+    component: PhotosComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { AboutComponent } from './component/about/about.component';
 import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
+import { PostsComponent } from './component/posts/posts.component';
+import {HttpClientModule} from '@angular/common/http';
+import { PhotosComponent } from './component/photos/photos.component';
+import { PostItemComponent } from './component/post-item/post-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    PostsComponent,
+    PhotosComponent,
+    PostItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
